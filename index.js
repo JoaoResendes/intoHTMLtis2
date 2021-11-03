61674374dcbfec5ac14683d3ab0538336147648d
Joao Resendes <joaoresendes2013@gmail.com>
2021-11-03 12:44:36 -0100

    alteração 03n0v2021

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('./public'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(_dirname,'/public/index.html'))
+})
+
+
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+app.get('/: nome', function (req, res) {
+    res.send(`Olá ${req.params.nome}`)
+  })
+ 
+app.listen(3000)
